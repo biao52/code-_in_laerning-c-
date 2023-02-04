bce55406ff282ed51d18f87ced0dcbdc2584f0b5
biao52 <112253999+biao52@users.noreply.github.com>
2023-02-04 20:08:20 +0800

    Update break跳出循环.c

--- break跳出循环.c
+++ break跳出循环.c
@@ -1,29 +1,29 @@
-//#include<stdio.h>
-//int main(void)
-//{
-//	int i, j, k = 0;
-//	for (i = 0; i <= 3; i++)
-//	{
-//		for (j = 0; j < 3; j++)
-//		{
-//			for (k = 0; k < 3; k++)
-//			{
-//				printf("Hhh\n");
-//				if (1 == k)
-//				{
-//					break;
-//				}
-//			}
-//			if (k = 1)
-//			{
-//				break;
-//			}
-//		}
-//			if (k = 1)
-//			{
-//				break;
-//			}//breakÖ»ÄÜÌø³öµ±Ç°ËùÔÚµÄÑ­»· ²»ÄÜÌø³öÈ«²¿µÄÑ­»·
-//		
-//	}
-//	return 0;
-//}
+#include<stdio.h>
+int main(void)
+{
+	int i, j, k = 0;
+	for (i = 0; i <= 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			for (k = 0; k < 3; k++)
+		{
+			printf("Hhh\n");
+				if (1 == k)
+				 {
+				break;
+			   }
+			}
+			if (k = 1)
+			{
+				break;
+			}
+	}
+			if (k = 1)
+			{
+				break;
+			}//breakåªèƒ½è·³å‡ºå½“å‰æ‰€åœ¨çš„å¾ªçŽ¯ ä¸èƒ½è·³å‡ºå…¨éƒ¨çš„å¾ªçŽ¯
+		
+	}
+	return 0;
+}
